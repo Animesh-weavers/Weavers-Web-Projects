f43a88c45a33f70ec736d9e77596bf455b77263e
ANIMESH DEY <deyanimesh4321@gmail.com>
2022-03-21 19:16:07 +0530

    update

--- dynamic-routing/src/Components/Details.js
+++ dynamic-routing/src/Components/Details.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 const Details = () => {
     const params=useParams();
+    console.log(params.userId)
     const userId=params.userId;
   return <div>Details about user {userId}</div>;
 };
